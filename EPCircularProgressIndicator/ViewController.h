1dfc87602fd58ca3c3b835031119657c413e1cb9
Eugene Pankratov <epankratov@gmail.com>
2015-09-03 11:04:36 +0300

    Initial Commit

---
+++ EPCircularProgressIndicator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EPCircularProgressIndicator
+//
+//  Created by Eugene Pankratov on 03.09.15.
+//  Copyright (c) 2015 pankratov.net.ua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
